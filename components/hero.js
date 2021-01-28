8b95dc17a9669f5a7f6333fc5359213cdf9b90e1
Armagan, Ugur <uarmagan@conversantmedia.com>
2021-01-28 10:32:36 -0600

    quick Hero edit

--- components/hero.js
+++ components/hero.js
@@ -24,7 +24,7 @@ export default function Hero() {
                                             <button type="submit" className="block w-full py-3 px-4 rounded-md shadow bg-gradient-to-r from-teal-500 to-cyan-600 text-white font-medium hover:from-teal-600 hover:to-cyan-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cyan-400 focus:ring-offset-gray-900">Let's Talk</button>
                                         </div>
                                     </div>
-                                    <p className="mt-3 text-sm text-gray-300 sm:mt-4">Enter your email and get a free consultation that includes a devops and infrastructure audit!</p>
+                                    <p className="mt-3 text-sm text-gray-300 sm:mt-4">Get a free consultation that includes a devops and infrastructure audit!</p>
                                 </form>
                             </div>
                         </div>
