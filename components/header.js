85f7e2d3bb38d7a7be9e49ac1a8ecddfa62ea0ec
Armagan, Ugur <uarmagan@conversantmedia.com>
2021-01-28 14:19:18 -0600

    Prettier changes

--- components/header.js
+++ components/header.js
@@ -1,19 +1,22 @@
 export default function Header() {
-    return (
-        <header className="relative">
-            <div className="bg-gray-900 pt-6">
-                <nav className="relative max-w-7xl mx-auto flex items-center justify-between px-4 sm:px-6" aria-label="Global">
-                    <div className="flex items-center flex-1">
-                        <div className="flex">
-                            <a href="#">
-                                <img className="h-8 w-auto sm:h-10" src="https://tailwindui.com/img/logos/workflow-mark-teal-200-cyan-400.svg" />
-                            </a>
-
-                        </div>
-                    </div>
-
-                </nav>
+  return (
+    <header className="relative">
+      <div className="bg-gray-900 pt-6">
+        <nav
+          className="relative max-w-7xl mx-auto flex items-center justify-between px-4 sm:px-6"
+          aria-label="Global">
+          <div className="flex items-center flex-1">
+            <div className="flex">
+              <a href="#">
+                <img
+                  className="h-8 w-auto sm:h-10"
+                  src="https://tailwindui.com/img/logos/workflow-mark-teal-200-cyan-400.svg"
+                />
+              </a>
             </div>
-        </header>
-    )
+          </div>
+        </nav>
+      </div>
+    </header>
+  );
 }
