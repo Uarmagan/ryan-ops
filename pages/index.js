b1b45c8c1252b9fb15cddb9225cf11776edeee81
Armagan, Ugur <uarmagan@conversantmedia.com>
2021-01-28 16:07:31 -0600

    separate blog component out into components folder

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,8 @@ import Header from '../components/header';
 import Hero from '../components/hero';
 import Features from '../components/features';
 import Testimonial from '../components/testimonial';
-import BlogItem from '../components/blog-item';
-import { blogData } from '../data/blog';
+import Blog from '../components/blog';
+
 export default function Home() {
   return (
     <>
@@ -13,31 +13,8 @@ export default function Home() {
           <main>
             <Hero />
             <Features />
-            {/* Testimonial section */}
             <Testimonial />
-            {/* Blog section */}
-            <div className="relative bg-gray-50 py-16 sm:py-24 lg:py-32">
-              <div className="relative">
-                <div className="text-center mx-auto max-w-md px-4 sm:max-w-3xl sm:px-6 lg:px-8 lg:max-w-7xl">
-                  <h2 className="text-base font-semibold tracking-wider text-cyan-600 uppercase">
-                    Learn
-                  </h2>
-                  <p className="mt-2 text-3xl font-extrabold text-gray-900 tracking-tight sm:text-4xl">
-                    Helpful Resources
-                  </p>
-                  <p className="mt-5 mx-auto max-w-prose text-xl text-gray-500">
-                    Learn more on the best practices and subject master
-                    knowledge from Ryan Ops through our detailed and educational
-                    posts.
-                  </p>
-                </div>
-                <div className="mt-12 mx-auto max-w-md px-4 grid gap-8 sm:max-w-lg sm:px-6 lg:px-8 lg:grid-cols-3 lg:max-w-7xl">
-                  {blogData.map((data) => (
-                    <BlogItem {...data} />
-                  ))}
-                </div>
-              </div>
-            </div>
+            <Blog />
           </main>
           <footer className="bg-gray-50" aria-labelledby="footerHeading">
             <div className="max-w-md mx-auto pt-5 px-4 sm:max-w-7xl sm:px-6 lg:pt-5 lg:px-8">
